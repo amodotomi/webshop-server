bf1eac903df3fd5a2fdc6bf779bb09ac073d42cf
amodotomi <amodotomi@gmail.com>
2023-06-13 18:13:38 +0300

    mariadb

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './users.model';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+  ) {}
+
+  findOne(filter: {
+    where: { id?: string; username?: string; email?: string };
+  }): Promise<User> {
+    return this.userModel.findOne({ ...filter });
+  }
+}
